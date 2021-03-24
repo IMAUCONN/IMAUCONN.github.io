d482002ab3c911cb5cf908c0c13a2eaf953f51fe
Jenna Mulett <jennamulett@gmail.com>
2021-03-23 21:50:37 -0400

    headshots + formatting

--- IMAUCONN.github.io/src/pages/about_us/index.tsx
+++ IMAUCONN.github.io/src/pages/about_us/index.tsx
@@ -4,6 +4,8 @@ import styles from './styles.module.css';
 import Link from '@docusaurus/Link';
 import clsx from 'clsx';
 import useBaseUrl from '@docusaurus/useBaseUrl';
+import { Container } from 'react-bootstrap';
+
 function Hello() {
 
   return (
@@ -13,7 +15,7 @@ function Hello() {
       <text className={styles.subtitle}>{"Who we are:" }</text> 
       <div className={styles.row}>
         <div className={styles.column}>
-          <p className={styles.paragraph}> The Information Management Association (IMA) is a student-run organization, at the University of Connecticut, that provides resources for students interested in the field of Information Technology. We allow students to develop their professional skill-set by connecting them to relevant job opportunities, all while creating an inviting community </p>
+          <p className={styles.paragraph}> The Information Management Association (IMA) is a student-run organization, at the University of Connecticut, that provides resources for students interested in the field of Information Technology. We allow students to develop their professional skill-set by connecting them to relevant job opportunities, all while creating an inviting community. </p>
           <p className={styles.paragraph}> Our meetings and events involve inviting IT representatives from various industries to connect with students and teach them about the industry, their specific companies, and potential job opportunities. We also focus on professional development by offering networking opportunities, resume building workshops, and mock interviews. </p>
           <p className={styles.paragraph}> Together we create a network of individuals passionate about information systems, analytics, and industry technology. </p>
         </div>
