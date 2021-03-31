2d2c78419b8cec808e0113eff4de1bcc50c812d7
Andrew Taylor <andrew.taylor.4378@gmail.com>
2021-03-31 19:09:53 +0000

    company relations form added

--- IMAUCONN.github.io/src/pages/company/index.tsx
+++ IMAUCONN.github.io/src/pages/company/index.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import Layout from '@theme/Layout';
+import { Container, Grid } from '@material-ui/core';
 
 function Hello() {
   return (
-    <Layout title="Hello">
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '50vh',
-          fontSize: '20px',
-        }}>
-        <p>
-          Edit <code>pages/hello.js</code> and save to reload.
-        </p>
-      </div>
+    <Layout>
+      <Container maxWidth='xl'>
+        <Grid container direction='column'>
+          <Grid item xs>
+            <h1>Recruitment</h1>
+          </Grid>
+          <Grid item xs>
+            <p>As part of the recruitment process, employers are strongly encouraged to host an information session which allows recruiters to informally meet potential candidates prior to their interviews. Information sessions also provide an opportunity for the employers to increase awareness of their company and additional career prospects.</p>
+          </Grid>
+          <Grid item xs>
+          <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScwv5p0ITxzB0Tt7OA-PTIvalQ5BVzbJtqTbUniQE77PQv0_A/viewform?embedded=true" width="640" height="650" frameBorder="0" marginHeight="0" marginWidth="0">Loading…</iframe>
+          </Grid>
+      </Grid>
+      </Container>
     </Layout>
   );
 }
