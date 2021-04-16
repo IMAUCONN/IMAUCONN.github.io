03c388ae7599f39daa920c9ef9fa39e562a4778a
Andrew Taylor <andrew.taylor.4378@gmail.com>
2021-04-16 17:20:13 +0000

    fixed company page

--- IMAUCONN.github.io/src/pages/company/index.tsx
+++ IMAUCONN.github.io/src/pages/company/index.tsx
@@ -35,7 +35,7 @@ function Hello() {
             <p>A big part of IMA is connecting our students with potential employers. As part of this recruitment process, we strongly encourage companies to host information sessions with our club. This allows recruiters to meet potential candidates prior to the interviewing process and increase awareness of their companies.</p>
           </Grid>
           <Grid item xs>
-          <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScwv5p0ITxzB0Tt7OA-PTIvalQ5BVzbJtqTbUniQE77PQv0_A/viewform?embedded=true" width="640" height="650" frameBorder="0">Loading…</iframe>
+          <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScwv5p0ITxzB0Tt7OA-PTIvalQ5BVzbJtqTbUniQE77PQv0_A/viewform?embedded=true" width="640" height="1500" frameBorder="0">Loading…</iframe>
           </Grid>
       </Grid>
       </Container>
