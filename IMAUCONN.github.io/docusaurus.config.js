fe900c324006114474c2ae2fcb6f7458654035dc
Jenna Mulett <jennamulett@gmail.com>
2021-04-16 12:09:32 -0400

    added logo pictures & got rid of switch

--- IMAUCONN.github.io/docusaurus.config.js
+++ IMAUCONN.github.io/docusaurus.config.js
@@ -5,15 +5,19 @@ module.exports = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/ima_logo_white.png',
   organizationName: 'IMAUCONN', // Usually your GitHub org/user name.
   projectName: 'IMAUCONN.github.io', // Usually your repo name.
   themeConfig: {
+    colorMode: {
+      disableSwitch: true
+    },
     navbar: {
       title: 'UConn IMA',
+      style: 'dark',
       logo: {
         alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        src: 'img/ima_logo.png',
       },
       items: [
         {
