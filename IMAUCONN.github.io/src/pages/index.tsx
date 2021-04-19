c7be1e836a511b3c1732d3b95458eb0fb242f435
Andrew Taylor <andrew.taylor.4378@gmail.com>
2021-04-19 20:07:55 +0000

    other comments

--- IMAUCONN.github.io/src/pages/index.tsx
+++ IMAUCONN.github.io/src/pages/index.tsx
@@ -6,6 +6,7 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import styles from './styles.module.css';
 
+ {/* This is where the two pictures are*/}
 const features = [
   {
     title: 'The Glabstein Lab',
@@ -16,7 +17,7 @@ const features = [
     imageUrl: 'img/headshots/eboard.jpg'
   },
 ];
-
+ {/* This is the Feature function that styles the images and puts them in divs */}
 function Feature({imageUrl, title}) {
   const imgUrl = useBaseUrl(imageUrl);
   return (
@@ -38,19 +39,22 @@ function Home() {
     <Layout
       title=""
       description="Description will go into a meta tag in <head />">
+        {/* This is where the color of the background*/}
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
+          {/* This is where the title and tagline is displayed they can be eddited in the docusaurus.config file*/}
           <h1 className="hero__title">{siteConfig.title}</h1>
           <p className="hero__subtitle">{siteConfig.tagline}</p>
         </div>
       </header>
+       {/* This is the join now button */}
       <div className={styles.buttons}>
             <Link style={{width:200}}
               className={clsx(
                 'button button--outline button--primary button--lg',
               )}
               to={useBaseUrl(
-                'https://docs.google.com/forms/d/e/1FAIpQLScZdMzhfu0VgPVrOcPrXRUVV17ijf9Mp2t0nZ_jBZv2r9JUxw/viewform'
+                'https://uconntact.uconn.edu/organization/ima'
               )}>
               Join Now
             </Link>
@@ -59,6 +63,7 @@ function Home() {
         {features && features.length > 0 && (
           <section className={styles.features}>
             <div className="container">
+               {/* This displays the pictures */}
               <div style={{alignItems: 'center', justifyContent: 'space-evenly'}} className="row">
                 {features.map((props, idx) => (
                   <Feature key={idx} {...props} />
