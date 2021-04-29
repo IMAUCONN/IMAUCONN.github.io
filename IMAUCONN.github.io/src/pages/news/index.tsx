1023bc68a81d9d7064817ac55a64a05f858779c9
Andrew Taylor <andrew.taylor.4378@gmail.com>
2021-04-29 22:30:20 +0000

    news feed started

--- IMAUCONN.github.io/src/pages/news/index.tsx
+++ IMAUCONN.github.io/src/pages/news/index.tsx
@@ -8,35 +8,36 @@ function strip(str: string) : string
 
 async function rssitems()
 {
-  console.log("refresing")
-  const res = await fetch(`https://api.allorigins.win/get?url=http://events.uconn.edu/api/rss/?calendar%5B%5D=608&calendar%5B%5D=283`);
+  const res = await fetch(`https://api.allorigins.win/get?url=http://www.business.uconn.edu/category/opim-innovate/feed`);
     const { contents } = await res.json();
-    console.log(contents);
     const feed = new window.DOMParser().parseFromString(contents, "text/xml");
     let items: any = feed.querySelectorAll("item");
     const feedItems = [...items].map((el) => ({
       link: el.querySelector("link").innerHTML,
-      location: el.querySelector("location").innerHTML,
+      description: el.querySelector("description").innerHTML,
       title: el.querySelector("title").innerHTML,
+      pubDate: el.querySelector("pubDate").innerHTML,
     }));
     return (feedItems);
 }
 export default function App() {
   const [items, setItems] = useState([]);
+  const f = (item) => {
+    return (
+      <div>
+        <h1>{strip(item.title)}</h1>
+        <p>{strip(item.description)}</p>
+         <p>{strip(item.pubDate)}</p> 
+        <a href={strip(item.link)}>{item.link}</a>
+      </div>
+    );
+  };
   
   return (
     <Layout title="News">
     <div className="App">
       <button onClick= {async () => {console.log("refresing");setItems(await rssitems())}}> refresh</button>
-      {items.map((item) => {
-        return (
-          <div>
-            <h1>{strip(item.title)}</h1>
-            <h1>{strip(item.location)}</h1>
-            <a href={strip(item.link)}>{item.link}</a>
-          </div>
-        );
-      })}
+      {items.map(f)}
     </div>
     </Layout>
   );
